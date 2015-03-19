1bdc25fb697fd354efb81922b2ee45593b1a1d06
MrVil <vilghost@gmail.com>
2015-03-19 09:27:08 +0100

    Card core

--- StupideVautour/GameCore/Cards/PlayerCard.cs
+++ StupideVautour/GameCore/Cards/PlayerCard.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace StupideVautour.GameCore.Cards
 {
-    class PlayerCard
+    public class PlayerCard
     {
+        Color color;
+        ushort value;
+
+        public PlayerCard(Color color, ushort value) {
+            this.color = color;
+            this.value = value;
+        }
+
+
+        Color Color
+        {
+            set { color = value; }
+            get { return color; }
+        }
+
+        short Value
+        {
+            set { this.value = value; }
+            get { return value; }
+        }
     }
 }
--- StupideVautour/GameCore/Cards/PointCard.cs
+++ StupideVautour/GameCore/Cards/PointCard.cs
@@ -1,11 +1,31 @@
-﻿cusing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace StupideVautour.GameCore.Cards
 {
-    class PointCard
+    public class PointCard
     {
+        CardType type;
+        short value;
+
+        PointCard(CardType type, short value)
+        {
+            this.type = type;
+            this.value = value;
+        }
+
+        CardType Type
+        {
+            set { type = value;}
+            get { return type; }
+        }
+
+        short Value {
+            set { this.value = value; }
+            get { return value; }
+        }
+
     }
 }
