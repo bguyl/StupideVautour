dffe3f82756440033c658781555f806e01009f5e
Mr.Vil <vilghost@gmail.com>
2015-04-27 23:22:49 +0200

    Error in UCB -> nbOfPlays should'nt be static

--- StupidVulture/GameCore/Players/AI_Tools/UCB.cs
+++ StupidVulture/GameCore/Players/AI_Tools/UCB.cs
@@ -7,24 +7,29 @@ using StupidVulture.GameCore.Cards;
 
 namespace StupidVulture.GameCore.Players.AI_Tools
 {
-    class UCB
+    public class UCB
     {
         private PlayerCard card;
         private int nbPlayed;
         private int nbWon;
-        private int confident = -1;
+        private int confident = 0;
         private int average;
         private int alpha;
 
         private static int nbOfPlays;
-        private static List<UCB> data = new List<UCB>();
-        private static UCB upperConfident;
+        /*public static List<UCB> data = new List<UCB>();
+        private static UCB upperConfident;*/
 
         public UCB(PlayerCard card, int parameter)
         {
             this.card = card;
             this.alpha = parameter;
-            data.Add(this);
+        }
+
+        public int NbWon
+        {
+            set { nbWon = value; }
+            get { return nbWon; }
         }
 
         public int Confident
@@ -54,19 +59,5 @@ namespace StupidVulture.GameCore.Players.AI_Tools
             average = nbWon / nbPlayed;
         }
 
-        public static UCB findUpperConfident()
-        {
-            UCB tmp = data[0];
-            foreach (UCB d in data)
-                if (tmp.Confident < d.Confident)
-                    tmp = d;
-            upperConfident = tmp;
-            return tmp;
-        }
-
-        public static void addWin()
-        {
-            upperConfident.nbWon++;
-        }
     }
 }
